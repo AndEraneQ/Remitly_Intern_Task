266fae4557d99ca59a2980d8005d40db6b3a9b4e
Piotr <trojanpiotrek@interia.pl>
2024-04-21 13:03:47 +0200

    updated main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         JsonReader jsonReader = new JsonReader();
         boolean result = jsonReader.checkIfResourceFieldContainsASingleAsterisk("src/main/resources/SampleJsonFile.json");
-        System.out.println("Result : " + result + " (false - there is a single asterisk, true - there is something diffrent" );
+        System.out.println("Result : " + result + " (false - there is a single asterisk, true - there is something diffrent)" );
     }
 }
